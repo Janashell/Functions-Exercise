106410c7f5017902b9c5e4b44f0896013b810d92
Janashell <janashell07@gmail.com>
2021-03-15 09:57:38 -0400

    Functions Exercise

---
+++ functionsExercise.js
@@ -0,0 +1,63 @@
+// 1
+function fozzieBear(){
+    console.log("Wocka, Wocka!");
+}
+fozzieBear();
+
+// 2
+// function beaker (){
+//     console.log("${meep}, ${meep}, ${meep}, ${meep}")
+// }
+// beaker();
+// 3
+// function werewolf(a, b)
+
+// 4
+function kermit(){
+    return "Kermit the Frog";
+}
+console.log(kermit());
+
+// 5
+// function muppetShowSeasons(seasons)
+
+// 6a
+const muppets = [
+    "Kermit The Frog", 
+    "Miss Piggy", 
+    "Fozzie Bear",  
+    "The Great Gonzo"
+];
+
+// 6b
+// function showMuppets(names){
+//     console.log(${names{0}}),
+// }
+
+// 7
+const manOrMuppet = function(){
+    console.log("Am I a man or am I a Muppet?");
+}
+manOrMuppet();
+
+// 8
+rainbowConnection = () => (
+    "Someday we'll find it. The Rainbow Connection. The lovers, the dreamers and me"
+)
+const rainbow = rainbowConnection();
+console.log(rainbow);
+
+// 9
+// no
+
+// 10
+// yes
+
+// 11a
+const newMuppetMovies = [
+    "The Muppets",
+    "Muppets Most Wanted"
+];
+
+// 11b
+const newMuppetMovies = 
