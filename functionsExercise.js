c529123cf3b9cbc540242057161ced7e7c938679
Janashell <janashell07@gmail.com>
2021-03-15 12:17:36 -0400

    Updated Exercise

--- functionsExercise.js
+++ functionsExercise.js
@@ -5,21 +5,38 @@ function fozzieBear(){
 fozzieBear();
 
 // 2
-// function beaker (){
-//     console.log("${meep}, ${meep}, ${meep}, ${meep}")
-// }
-// beaker();
-// 3
-// function werewolf(a, b)
-
-// 4
-function kermit(){
-    return "Kermit the Frog";
+function beaker (speak){
+    console.log(speak) 
+    console.log(speak), 
+    console.log(speak),
+    console.log(speak)
 }
+beaker(`Meep`);
+3
+function werewolf(a, b){
+    if (a === `Muppet` && b === `Show`){
+        console.log(`It's time to play the music. It's time to light the lights.`);
+    }
+}
+werewolf(`Muppet`, `Show`);
+
+// 4  
+// function kermit(){
+//     return "Kermit the Frog";
+// }
+// console.log(kermit());
+
+kermit = () => `Kermit the Frog`;  //Best way to do it
 console.log(kermit());
 
 // 5
-// function muppetShowSeasons(seasons)
+function muppetShowSeasons(seasons){
+    if (seasons === 5){
+        return true
+    }
+    return false
+}
+console.log(muppetShowSeasons(5));
 
 // 6a
 const muppets = [
@@ -30,9 +47,13 @@ const muppets = [
 ];
 
 // 6b
-// function showMuppets(names){
-//     console.log(${names{0}}),
-// }
+function showMuppets(names){
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
+}
+showMuppets(muppets);
 
 // 7
 const manOrMuppet = function(){
@@ -60,4 +81,69 @@ const newMuppetMovies = [
 ];
 
 // 11b
-const newMuppetMovies = 
+function toUpperMovie(movie){
+    return movie.toUpperCase();
+}
+const upperMovies = newMuppetMovies.map(toUpperMovie);
+console.log(upperMovies);
+
+// 12a
+const oldMuppetMovies = [
+    "The Muppet Movie", 
+    "The Muppets Take Manhattan", 
+    "The Great Muppet Caper", 
+    "The Muppet Christmas Carol", 
+    "Muppet Treasure Island",  
+    "Muppets From Space"
+]
+
+// 12b
+const twoMovies = oldMuppetMovies.filter(title => title === `The Great Muppet Caper` || title === `Muppet Treasure Island`);
+console.log(twoMovies);
+
+// BONUS
+
+// 13a
+const charactersOne = ["Statler", "Waldorf"];
+
+// 13b
+const charactersTwo = [
+    "The Swedish Chef", 
+    "Animal",  
+    "Rowlf"
+];
+
+// 13c
+function randomMuppet(characters){
+    console.log(characters[Math.floor(Math.random() * characters.length)]);
+}
+
+// 13d
+randomMuppet(charactersOne);
+randomMuppet(charactersTwo);
+
+// 14
+function isLetter(str){
+    return str.length === 1 && str.match(/[a-z]/i)
+}
+function caps(string){
+    let newString = "";
+    let n = 0;
+    for (letter of string){
+        if (isLetter(letter)){
+            if (n % 2 === 0){
+                newString += letter.toUpperCase ();
+                n++;
+            } else {
+                newString += letter.toLowerCase();
+                n++;
+            }
+        } else {
+            newString += letter;
+        }
+    }
+    return newString;
+}
+console.log(caps(`I Love Muppets`));
+
+
